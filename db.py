099c8cdc7e9b025c63a96f79f587c2211eec73f9
Hooman Shahidi <hooman.ksh@gmail.com>
2026-06-17 01:32:16 -0400

    fix

--- db.py
+++ db.py
@@ -3,7 +3,6 @@ import sqlite3, os
 # DB_PATH = os.path.join(os.path.dirname(__file__), "apex.db")
 DB_PATH = os.environ.get("DB_PATH", os.path.join(os.path.dirname(__file__), "apex.db"))
 
-
 def get_db():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
